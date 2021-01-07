a9336e0cad0bdcfd0b79c3cb68e6040461b76175
Arhaan Bahadur <arhaanb@gmail.com>
2021-01-07 21:47:15 +0530

    😚

--- src/title.svelte
+++ src/title.svelte
@@ -1,16 +1,16 @@
 <div>
-  <h1 class="title">A Shitty Weather App</h1>
+  <h1 class="title">A Weather App</h1>
   <div class="row">
     <div class="seven columns">
       <p>
-        This is a shitty weather app. Enter city to get ur weather. This
+        This is a weather app. Enter city to get ur weather. This
         application uses the
         <a href="https://openweathermap.org/api" target="_blank">Open Weather
           API</a>
         and
         <span><a href="https://developer.here.com/" target="_blank">Here Maps
             API</a>.</span>
-        The map takes a second to load because the API kinda slow :/
+        The map takes a second to load because the API is kinda slow :/
       </p>
     </div>
   </div>
